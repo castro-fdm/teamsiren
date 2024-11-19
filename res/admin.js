dabf0585e9e986356b3611b6700f2efb08ee45e7
naysu06 <franzdominicc@gmail.com>
2024-11-19 22:29:35 +0800

    first commit

---
+++ res/admin.js
@@ -0,0 +1,15 @@
+function displayReservationStatus() {
+    const infoCategory = document.querySelector('.info-category');
+    infoCategory.innerHTML = `
+      <h2>Reservation Status</h2>
+      <p>Here is some information about reservation status.</p>
+    `;
+  }
+  
+  function displayServices() {
+    const infoCategory = document.querySelector('.info-category');
+    infoCategory.innerHTML = `
+      <h2>Services</h2>
+      <p>Here is some information about services.</p>
+    `;
+  }
